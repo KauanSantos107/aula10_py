0f1dd3f917233399382e8d1b9afcc43f63d206cb
KauanSantos <kauangoll62@gmail.com>
2026-05-04 10:11:00 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,9 +1,7 @@
-# Inicializamos a variável que guardará o total da soma
 soma_pares = 0
-# Criamos uma lista vazia para armazenar os números e apresentá-los depois
 lista_pares = []
 
-# O range começa em 2, vai até 501 (para incluir o 500) e pula de 2 em 2
+
 for numero in range(2, 501, 2):
     lista_pares.append(numero)
     soma_pares += numero
