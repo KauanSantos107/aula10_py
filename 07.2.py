2be88e024ea222bdf511fe157716e083996d5317
KauanSantos <kauangoll62@gmail.com>
2026-05-04 08:16:37 -0300

    Create 07.2.py

---
+++ 07.2.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome completo: ")
+
+
+for letra in nome:
+    if letra == " ":
+        break
+    print(letra)
+for letra in nome:
+    if letra == " ":
+        break
+        
+    print(letra, end="")
